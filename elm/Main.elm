68cbc5173f8389697749adf901d2c26013f7416d
cm <cm@centimentalcomics.com>
2021-01-23 13:09:25 +0800

    main and init

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,8 +1,13 @@
 module Main exposing (..)
 
+import Browser exposing (Document)
 import Html exposing (text)
 
 main =
     Browser.application
         { init = init
         }
+
+init : Float -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init version url key =
+    updateUrl url { page = NotFound, key = key, version = version }
