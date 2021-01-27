2679d485a519093c7c4ec1c76042a2c60300579c
cm <cm@centimentalcomics.com>
2021-01-27 13:26:57 +0800

    url parser

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Browser exposing (Document)
 import Browser.Navigation as Nav
 import Html exposing (text)
 import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser, s, string)
 
 type alias Model =
     { page : Page
@@ -18,6 +19,13 @@ type Msg
     = ClickedLink Browser.UrlRequest
 
 
+parser : Parser (Route -> a) a
+parser =
+    Parser.oneOf
+        [
+        ]
+
+
 main =
     Browser.application
         { init = init
@@ -27,3 +35,10 @@ main =
 init : Float -> Url -> Nav.Key -> ( Model, Cmd Msg )
 init version url key =
     updateUrl url { page = NotFound, key = key, version = version }
+
+
+updateUrl : Url -> Model -> ( Model, Cmd Msg )
+updateUrl url model =
+    case Parser.parse parser url of
+        Nothing ->
+            ( { model | page = NotFound }, Cmd.none )
