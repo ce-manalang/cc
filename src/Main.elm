4f57c799329e597122d29cb174c2f1b6568beee4
cm <cm@centimentalcomics.com>
2021-02-02 12:51:39 +0800

    nav

--- src/Main.elm
+++ src/Main.elm
@@ -2,7 +2,8 @@ module Main exposing (..)
 
 import Browser exposing (Document)
 import Browser.Navigation as Nav
-import Html exposing (text)
+import Html exposing (Html, a, footer, h1, li, nav, text, ul)
+import Html.Lazy exposing (lazy)
 import Url exposing (Url)
 import Url.Parser as Parser exposing ((</>), Parser, s, string)
 
@@ -19,6 +20,56 @@ type Page
 
 type Route
     = Stories
+
+
+view : Model -> Document Msg
+view model =
+    let
+        content =
+            case model.page of
+                NotFound ->
+                    text "Not Found"
+    in
+    { title = "centimentalcomics"
+    , body =
+        [ lazy viewHeader model.page
+        , content
+        , viewFooter
+        ]
+    }
+
+
+viewHeader : Page -> Html Msg
+viewHeader page =
+    let
+        logo =
+            h1 [] [ text "centimentalcomics" ]
+
+        links =
+            ul []
+                -- [ navLink Home { url = "/", caption = "Home" }
+                -- , navLink About { url = "/about", caption = "About" }
+                -- ]
+
+        -- navLink : Route -> { url : String, caption : String } -> Html msg
+        -- navLink route { url, caption } =
+        --     li
+        --        [ classList
+        --            [ ( "active"
+        --              , isActive { link = route, page = page }
+        --              )
+        --            ]
+        --        ]
+        --        [ a [ href url ] [ text caption ] ]
+    in
+    -- nav [] [ logo, links ]
+    nav [] [ logo ]
+
+
+viewFooter : Html msg
+viewFooter =
+    footer []
+        [ text "One is never alone with a rubber duck. -Douglas Adams" ]
 
 
 type Msg
@@ -50,6 +101,7 @@ main =
         { init = init
         , subscriptions = subscriptions
         , update = update
+        , view = view
         }
 
 
